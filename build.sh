1d49d6f956f60b1dae771d1e0d7b341fd4d7726c
Steffen Klee <steffen.klee@gmail.com>
2019-10-10 10:47:05 +0200

    Added CEF docker image to build script

--- build.sh
+++ build.sh
@@ -6,3 +6,6 @@ docker build --no-cache -t viaduck/ci:mingw -f Dockerfile.mingw .
 
 # Android image
 docker build --no-cache -t viaduck/ci:android -f Dockerfile.android .
+
+# CEF image
+docker build --no-cache -t viaduck/cef -f Dockerfile.cef .
