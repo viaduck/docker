f82ca43e43a3c9f66529ed00b0ada50e0e75c3bb
Alexandros Roussos <thealexroussos@gmail.com>
2021-11-17 02:12:01 +0100

    Updated all images
    * Updated base image to debian bullseye
    * Better gpg key management
    * Removed unneeded fixes
    * Removed unused CEF image

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+# General CI image
 docker build --build-arg MIRROR=http://mirror.hetzner.de/debian --no-cache -t viaduck/ci -f Dockerfile.ci .
 
 # MinGW image
@@ -7,5 +9,3 @@ docker build --no-cache -t viaduck/ci:mingw -f Dockerfile.mingw .
 # Android image
 docker build --no-cache -t viaduck/ci:android -f Dockerfile.android .
 
-# CEF image
-docker build --build-arg MIRROR=http://mirror.hetzner.de/ubuntu --no-cache -t viaduck/cef -f Dockerfile.cef .
