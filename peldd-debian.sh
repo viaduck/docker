4872bbd75027cd1544f8c4f700accda37fb0e69e
Alexandros Roussos <thealexroussos@gmail.com>
2022-02-04 04:40:25 +0100

    Updated mingw-w64 version in peldd

--- peldd-debian.sh
+++ peldd-debian.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-peldd-bin -a -p "/usr/x86_64-w64-mingw32/lib" -p "/usr/x86_64-w64-mingw32/bin" -p "/usr/lib/gcc/x86_64-w64-mingw32/8.3-posix/" --clear-path $@
+peldd-bin -a -p "/usr/x86_64-w64-mingw32/lib" -p "/usr/x86_64-w64-mingw32/bin" -p "/usr/lib/gcc/x86_64-w64-mingw32/10-posix/" --clear-path $@
 
