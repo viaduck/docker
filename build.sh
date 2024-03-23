2e252fa69e5af9749d90201c69a0c1ed7ffd84e6
Alexandros Roussos <thealexroussos@gmail.com>
2024-03-23 05:00:21 +0100

    Refactored apt commands and folder structure
    * Changed all apt install usages to reflect the usage of packages
      in-line, without duplicating the package names at the top and getting
      out-of-sync quickly when only changing the install directive
    * Moved to cleaner folder structure

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # General CI image
-docker build --build-arg MIRROR=http://mirror.hetzner.de/debian --no-cache -t viaduck/ci -f Dockerfile.ci .
+docker build --build-arg MIRROR=http://mirror.hetzner.de/debian --no-cache -t viaduck/ci -f docker/Dockerfile.ci .
 
 # MinGW image
-docker build --no-cache -t viaduck/ci:mingw -f Dockerfile.mingw .
+docker build --no-cache -t viaduck/ci:mingw -f docker/Dockerfile.mingw .
 
 # Android image
-docker build --no-cache -t viaduck/ci:android -f Dockerfile.android .
+docker build --no-cache -t viaduck/ci:android -f docker/Dockerfile.android .
 
 # GUI image
-docker build --no-cache -t viaduck/ci:gui -f Dockerfile.gui .
+docker build --no-cache -t viaduck/ci:gui -f docker/Dockerfile.gui .
